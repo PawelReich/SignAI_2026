c6c1a7bc1f75f0a87de5ac44bfa4c42524ceeb9c
Pawel Reich <thegitman@outlook.com>
2026-03-22 04:57:01 +0100

    rm debug

--- stm32_project/Debug/Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_api.d
+++
@@ -1,8 +0,0 @@
-Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_api.o: \
- ../Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_api.c \
- ../Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_api.h \
- ../Drivers/BSP/Components/vl53l8cx/porting/platform.h \
- ../Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_buffers.h
-../Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_api.h:
-../Drivers/BSP/Components/vl53l8cx/porting/platform.h:
-../Drivers/BSP/Components/vl53l8cx/modules/vl53l8cx_buffers.h:
--- stm32_project/Debug/Drivers/BSP/Components/vl53l8cx/porting/platform.d
+++
@@ -1,4 +0,0 @@
-Drivers/BSP/Components/vl53l8cx/porting/platform.o: \
- ../Drivers/BSP/Components/vl53l8cx/porting/platform.c \
- ../Drivers/BSP/Components/vl53l8cx/porting/platform.h
-../Drivers/BSP/Components/vl53l8cx/porting/platform.h:
